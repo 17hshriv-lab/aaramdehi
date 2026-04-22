5f176e0c33f62d007a47139a6082693167f92062
livemehimanshu <46988939+livemehimanshu@users.noreply.github.com>
2026-04-23 00:47:26 +0530

    Corrected vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'; // 1. Import yahan karo
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HelmetProvider> {/* 2. Pure App ko yahan wrap karo */}
+      <App />
+    </HelmetProvider>
   </StrictMode>,
 )
